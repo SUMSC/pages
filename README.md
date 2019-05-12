f5e6e01f2bb94a64dde619caae8619761cfe09aa
wzhzzmzzy <wzhzzmzzy@gmail.com>
2019-05-12 11:40:10 +0800

    edit deploy

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# pages
-vuepress content page for sumsc.github.io
+# SUMSC Documents Page
+
+SUMSC.github.io构建源码仓库。本项目使用 [Vuepress 1.x](https://v1.vuepress.vuejs.org/zh/) 构建。
+
+## 起步
+
+首先你需要拥有`node`和`npm`。
+
+```shell
+git clone https://github.com/SUMSC/pages.git
+cd pages
+npm i
+```
+
+### 预览
+
+```shell
+npm run home:dev
+```
+
+### 构建
+
+```shell
+npm run home:build
+```
+
+### 部署
+
+```shell
+bash ./deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,12 @@ set -e
 
 npm run home:build # 生成静态文件
 cd home/.vuepress/dist # 进入生成的文件夹
-# echo 'www.sumsc.xin' > CNAME # 发布到自定义域名
+echo 'sumsc.xin' > CNAME # 发布到自定义域名
+git config --global user.email "wzhzzmzzy@gmail.com"
+git config --global user.name "wzhzzmzzy"
 git init
 git add -A
 git commit -m 'deploy home'
-git push -f  'https://github.com/SUMSC/SUMSC.github.io.git' master # 发布到 https://SUMSC.github.io
+git push -f 'https://github.com/SUMSC/SUMSC.github.io.git' master # 发布到 https://SUMSC.github.io
 
 cd -
